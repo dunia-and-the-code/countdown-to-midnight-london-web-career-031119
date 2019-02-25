ca5c0b491eb2c078d8905f2bf7b2f8b336a8c76c
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-25 00:22:04 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
-
-def countdown
+def countdown_with_sleep(int)
+  while int > 0 
+  puts "#{int} SECOND(S)!" 
+  sleep 5
+  int -= 1 
+  end
 end
+
+
+def countdown(numbers)
+  while numbers > 0 
+  puts "#{numbers} SECOND(S)!"
+  numbers -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+
+  
+
+
